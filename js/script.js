32ebfd970f8c965b6cf568eba94793c357bc36ab
Zookerman3 <szook7@gmail.com>
2023-01-23 15:16:44 -0800

    input values display in form after submission
    Co-authored-by: Charles <charlesbcookman@gmail.com>

--- js/script.js
+++ js/script.js
@@ -32,21 +32,38 @@ function Task(taskName, taskTime, taskPerson, taskSupplies, taskLocation, taskDa
 
 //UI Logic
 
-function addToChecklist(name, date, person, supplies, location,) {
-    let inputForm = document.querySelector("inputform");
+function addToChecklist(name, time, person, supplies, location, due) {
+    let inputform = document.querySelector("#inputform");
     let newForm = document.createElement("form");
     let label = document.createElement("label");
     let checkbox = document.createElement("input");
     let ul = document.createElement("ul");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let li3 = document.createElement("li");
+    let li4 = document.createElement("li");
+    let li5 = document.createElement("li");
+    let button = document.createElement("button");
+    let p = document.createElement("p");
+    document.getElementById("inputform").append(p);
+    inputform.after(newForm);
+    newForm.append(label);
+    label.after(checkbox);
+    checkbox.after(ul);
+    ul.after(button);
+    li1.innerText = time + "minutes";
+    li2.innerText = person;
+    li3.innerText = supplies;
+    li4.innerText = location;
+    li5.innerText = "Due By:" + due;
+    ul.append(li1, li2, li3, li4, li5);
     newForm.setAttribute("id", name);
-    label.innerText() = name;
+    label.innerText = name;
+    label.setAttribute("for", name + due);
     checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", name + date);
-    ul.setAttribute
-    newForm.after(inputForm);
-
+    checkbox.setAttribute("id", name + due);
+    
 }
-
 let list = new List();
 
 function handleFormSubmission(event){
@@ -61,7 +78,8 @@ function handleFormSubmission(event){
     let newTask = new Task(inputtedName, inputtedTime, inputtedPerson, inputtedSupplies, inputtedLocation, inputtedDueDate, false);
     list.addTask(newTask);
     console.log(list.tasks);
-    document.querySelector("#inputform").reset();
+    //document.querySelector("#inputform").reset();
+    addToChecklist(inputtedName, inputtedTime, inputtedPerson, inputtedSupplies, inputtedLocation);
 }
 
 window.addEventListener("load", function(event){
