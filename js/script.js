4e7a2d3508b0d4a17e3ac9d5893b67d70c6e6898
Zookerman3 <szook7@gmail.com>
2023-01-23 11:12:24 -0800

    added business logic
    Co-authored-by: Charles <charlesbcookman@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function List(){
+    this.tasks = {};
+}
+
+List.prototype.addTask = function(task){
+    this.tasks[task.taskName] = task;
+}
+
+List.prototype.assignPerson = function(person, task){
+    task.taskPerson = person;
+    return task.taskPerson;
+}
+
+List.prototype.checkIt = function(task) {
+    if (task.taskComplete === true) {
+        delete this.task;
+        return "deleted";
+    }
+}
+
+function Task(taskName, taskTime, taskPerson, taskSupplies, taskLocation, taskDate, taskComplete){
+    this.taskName = taskName;
+    this.taskTime = taskTime;
+    this.taskPerson = taskPerson;
+    this.taskSupplies = taskSupplies;
+    this.taskLocation = taskLocation;
+    this.taskDate = taskDate;
+    this.taskComplete = taskComplete
+}
